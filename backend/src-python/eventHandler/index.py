0ae0d92f9a2388aae6c3a8dbc0c881665cfeac70
Alexander Van de Kleut <54694031+avandekleut@users.noreply.github.com>
2022-09-14 16:41:05 -0400

    add ability to run locally

--- backend/src-python/eventHandler/index.py
+++ backend/src-python/eventHandler/index.py
@@ -41,10 +41,49 @@ def handler(event, context):
     try:
         response = s3.get_object(Bucket=bucket, Key=key)
         print("CONTENT TYPE: " + response['ContentType'])
-        print(json.dumps(response, indent=2))
         return response['ContentType']
     except Exception as e:
         print(e)
         print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
         raise e
               
+if __name__ == '__main__':
+    event = {
+        "Records": [
+            {
+            "eventVersion": "2.0",
+            "eventSource": "aws:s3",
+            "awsRegion": "us-east-1",
+            "eventTime": "1970-01-01T00:00:00.000Z",
+            "eventName": "ObjectCreated:Put",
+            "userIdentity": {
+                "principalId": "EXAMPLE"
+            },
+            "requestParameters": {
+                "sourceIPAddress": "127.0.0.1"
+            },
+            "responseElements": {
+                "x-amz-request-id": "EXAMPLE123456789",
+                "x-amz-id-2": "EXAMPLE123/5678abcdefghijklambdaisawesome/mnopqrstuvwxyzABCDEFGH"
+            },
+            "s3": {
+                "s3SchemaVersion": "1.0",
+                "configurationId": "testConfigRule",
+                "bucket": {
+                "name": "presigned-url-dev-presignedurldev997733e0-pn9lk5uf6o0l",
+                "ownerIdentity": {
+                    "principalId": "EXAMPLE"
+                },
+                "arn": "arn:aws:s3:::presigned-url-dev-presignedurldev997733e0-pn9lk5uf6o0l"
+                },
+                "object": {
+                "key": "f45631ee-bbaa-4646-8792-56c3b60d1e87/preferences.xlsx",
+                "size": 277,
+                "eTag": "7c6dfc0d3c9caad25546a2d2791a5d45",
+                }
+            }
+            }
+        ]
+        }
+    response = handler(event, None)
+    print({response})
